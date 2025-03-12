8296cc22ceae24fd601844e8ed95252a4943ba7e
FatouNdiay <ndiayefatou101123@gmail.com>
2025-03-12 12:22:47 +0100

    message

---
+++ config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                rouge: '#da373d',
+                bleu: '#181E4B',
+                orange: '#F1A501',
+                blanc: '#fff',
+                rose: '#DF6951',
+                violet:'#8A79DF',
+                gris:'#5E6282',
+                claire:'#DFD7F9',
+                jaune:'#FF7D68',
+                indigo:'#5E3BE1',
+                ciel:'#59B1E6',
+               
+            },
+        }
+    }
+}
